d523e86ed23a320bae1c3ea734c2c964b344ef80
Ranjodh Singh <165930034+ranjodh-s@users.noreply.github.com>
2026-07-04 22:47:27 +0530

    Fix environment variable reference for database URL

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,7 +6,7 @@ dotenv.config();
 const { Pool } = pg;
 
 const pool = new Pool({
-     connectionString: process.DATABASE_URL,
+     connectionString: process.env.DATABASE_URL,
 });
 
 pool.connect()
